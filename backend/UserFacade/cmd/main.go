0bfa729a1d5f1ffea3f3fc31f3ec7081cff09935
Aydar Sirazeev <aisirazeev@avito.ru>
2022-01-24 02:28:40 +0300

    SIF-15: fixed main.go

--- backend/UserFacade/cmd/main.go
+++ backend/UserFacade/cmd/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
-	_ "UserFacade/cmd/docs"
+	_ "UserFacade/docs"
+	"UserFacade/internal/clients/auth_service"
+	"UserFacade/internal/clients/profile_service"
+	"UserFacade/internal/rpc/get_all_profiles"
+	"UserFacade/internal/rpc/login"
+	"UserFacade/internal/rpc/logout"
+	"UserFacade/internal/rpc/register"
+	"github.com/sirupsen/logrus"
 
 	//"UserFacade/internal/clients/auth_service"
 	//"UserFacade/internal/clients/profile_service"
@@ -18,35 +25,34 @@ import (
 // @host https://localhost:8080
 // @BasePath /
 func main() {
-	//logger := logrus.New()
+	logger := logrus.New()
 
-	//logger.Info("Initializing dependent services")
-	//authServiceClient, err := auth_service.NewClientFromEnv()
-	//if err != nil {
-	//	logger.Errorf("cannot connect to auth service: %s", err.Error())
-	//	return
-	//}
-	//profileServiceClient, err := profile_service.NewClientFromEnv()
-	//if err != nil {
-	//	logger.Errorf("cannot connect to profile service: %s", err.Error())
-	//	return
-	//}
-	//logger.Info("Dependent services initializing finished")
-	//
-	//loginHandler := login.NewHandler(authServiceClient, profileServiceClient, logger)
-	//logoutHandler := logout.NewHandler(authServiceClient, logger)
-	//registerHandler := register.NewHandler(authServiceClient, profileServiceClient, logger)
-	//getAllProfilesHandler := get_all_profiles.NewHandler(profileServiceClient, logger)
-	////
+	logger.Info("Initializing dependent services")
+	authServiceClient, err := auth_service.NewClientFromEnv()
+	if err != nil {
+		logger.Errorf("cannot connect to auth service: %s", err.Error())
+		return
+	}
+	profileServiceClient, err := profile_service.NewClientFromEnv()
+	if err != nil {
+		logger.Errorf("cannot connect to profile service: %s", err.Error())
+		return
+	}
+	logger.Info("Dependent services initializing finished")
+
+	loginHandler := login.NewHandler(authServiceClient, profileServiceClient, logger)
+	logoutHandler := logout.NewHandler(authServiceClient, logger)
+	registerHandler := register.NewHandler(authServiceClient, profileServiceClient, logger)
+	getAllProfilesHandler := get_all_profiles.NewHandler(profileServiceClient, logger)
 	handler := http.NewServeMux()
-	//handler.HandleFunc("/login", loginHandler.Handle)
-	//handler.HandleFunc("/register", registerHandler.Handle)
-	//handler.HandleFunc("/logout", logoutHandler.Handle)
-	//handler.HandleFunc("/profiles", getAllProfilesHandler.Handle)
+	handler.HandleFunc("/login", loginHandler.Handle)
+	handler.HandleFunc("/register", registerHandler.Handle)
+	handler.HandleFunc("/logout", logoutHandler.Handle)
+	handler.HandleFunc("/profiles", getAllProfilesHandler.Handle)
 	handler.HandleFunc("/docs", httpSwagger.Handler())
 
 	handlers := middleware.SetupMiddleware(handler)
-	err := http.ListenAndServe(":8080", handlers)
+	err = http.ListenAndServe(":8080", handlers)
 	if err != nil {
 		return
 	}
