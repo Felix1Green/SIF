f2aa1851de3d44776f596f3a442fd1a5d0dca181
Aydar Sirazeev <aisirazeev@avito.ru>
2022-02-09 21:08:06 +0300

    SIF-20: added new entities

--- backend/ChatService/internal/entities/message/entity.go
+++ backend/ChatService/internal/entities/message/entity.go
@@ -1,10 +1,32 @@
 package message
 
+type ContentType int
+type MsgDirection string
+
+const (
+	TextPlain ContentType = iota
+	TextMarkdown
+	TextHtml
+	Image
+	Gallery
+	Kml
+	Attachment
+	AttachmentList
+	Video
+	VCard
+	Other = 255
+)
+
+const (
+	Incoming MsgDirection = "incoming"
+	OutGoing MsgDirection = "outgoing"
+)
+
 type Message struct {
-	Id          int64
-	Status      string
-	ContentType string
-	SenderId    int64
-	Direction   string
-	Content     string
+	ConversationId   int64        `json:"conversationId"`
+	Id               int64        `json:"id"`
+	SenderId         int64        `json:"senderId"`
+	Content          string       `json:"content"`
+	ContentType      ContentType  `json:"contentType"`
+	MessageDirection MsgDirection `json:"messageDirection"`
 }
